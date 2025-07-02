042015de6a901e7f5a70e3eb316955406144069b
Ana Luiza <analu.tibi@outlook.com>
2025-07-02 01:04:14 -0300

    3.1 Criando funcionalidades testes

--- GoTravel/src/scripts/script.js
+++ GoTravel/src/scripts/script.js
@@ -1,8 +1,23 @@
 const login = document.getElementById('btn-login')
+const compra = document.getElementById('btn-compra')
+let email = ''
+let passagem = ''
+
 login.onclick = function () {
-    let email = prompt("Digite seu email para fazer login:")
+    email = prompt("Digite seu email para fazer login:")
     while(email.trim() === '') {
         email = prompt("Digite seu email para fazer login:")
     }
     alert(`O seu email é: ${email}`)
+}
+
+compra.onclick = function() {
+    if(email.trim() == '') {
+        alert('Faça login primeiro')
+    } else {
+        passagem = prompt(`${email}\nQuantas passagens você quer comprar?`)
+        while(passagem.trim() == '') {
+            passagem = prompt(`${email}\nQuantas passagens você quer comprar?`)
+        } alert(`${email}\nComprou ${passagem}!!`)
+    }
 }
